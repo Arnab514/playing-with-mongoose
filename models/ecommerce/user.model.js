054e9776443e0b3825ff9ff4fdcfda5851931b42
Arnab514 <arnabmajumdar399@gmail.com>
2024-03-05 02:06:42 +0530

    changes

---
+++ models/ecommerce/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+export const User = mongoose.model('User' , userSchema) 
